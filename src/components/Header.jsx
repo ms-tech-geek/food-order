743f8a22d98d8b6d63cc3fde11de2c3851482bea
ms-tech-geek <thecorporator@gmail.com>
2024-08-28 17:46:38 +0530

    usingCartContext and adding items to cart and displaying count of total items in header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,18 +1,26 @@
-import logoImg from '../assets/logo.jpg'
-import Button from './UI/Button';
+import { useContext } from "react";
+import logoImg from "../assets/logo.jpg";
+import Button from "./UI/Button";
+import CartContext from "../store/CartContext";
 
 const Header = () => {
-    return (
-        <header id="main-header">
-            <div id="title">
-                <img src={logoImg} alt={`A restaurant`}/>
-                <h1>ReactFood</h1>
-            </div>
-            <nav>
-                <Button textOnly>Cart (0)</Button>
-            </nav>
-        </header>
-    );
+	const { items } = useContext(CartContext);
+
+	const totalCartItems = items.reduce(
+		(totalNumberOfItems, item) => totalNumberOfItems + item.quantity,
+		0
+	);
+	return (
+		<header id="main-header">
+			<div id="title">
+				<img src={logoImg} alt={`A restaurant`} />
+				<h1>ReactFood</h1>
+			</div>
+			<nav>
+				<Button textOnly>Cart ({totalCartItems})</Button>
+			</nav>
+		</header>
+	);
 };
 
 export default Header;
