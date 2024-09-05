a1783265489e09e8a7e7362648e1ae16ad65ae0a
ms-tech-geek <thecorporator@gmail.com>
2024-09-05 15:38:58 +0530

    BugFix

--- src/hooks/useHttp.js
+++ src/hooks/useHttp.js
@@ -1,48 +1,53 @@
-import { useEffect, useState, useCallback } from "react";
+import { useCallback, useEffect, useState } from "react";
 
-const sendHttpRequest = async ( url, config ) => {
+async function sendHttpRequest(url, config) {
 	const response = await fetch(url, config);
-	const respData = await response.json();
+
+	const resData = await response.json();
 
 	if (!response.ok) {
 		throw new Error(
-			respData.message || `Something went wrong, failed to send http request.`
+			resData.message || "Something went wrong, failed to send request."
 		);
 	}
 
-	return respData;
-};
+	return resData;
+}
 
-const useHttp = ({ url, config, initialData }) => {
+export default function useHttp(url, config, initialData) {
 	const [data, setData] = useState(initialData);
 	const [isLoading, setIsLoading] = useState(false);
 	const [error, setError] = useState();
 
-	const sendRequest = useCallback(async (data) => {
-		setIsLoading(true);
-		try {
-			const respData = await sendHttpRequest( url, {...config, body:data} );
-			setData(respData);
-		} catch (error) {
-			setError(error.message || `Something went wrong!`);
-		}
-		setIsLoading(false);
-	}, [url, config]);
+	function clearData() {
+		setData(initialData);
+	}
+
+	const sendRequest = useCallback(
+		async function sendRequest(data) {
+			setIsLoading(true);
+			try {
+				const resData = await sendHttpRequest(url, { ...config, body: data });
+				setData(resData);
+			} catch (error) {
+				setError(error.message || "Something went wrong!");
+			}
+			setIsLoading(false);
+		},
+		[url, config]
+	);
 
 	useEffect(() => {
-		if (config && (config.method === "GET" || !config.method || !config))
-			{
-				sendRequest();
-
-			}
-	}, [sendRequest,config]);
+		if ((config && (config.method === "GET" || !config.method)) || !config) {
+			sendRequest();
+		}
+	}, [sendRequest, config]);
 
 	return {
 		data,
 		isLoading,
 		error,
 		sendRequest,
+		clearData,
 	};
-};
-
-export default useHttp;
+}
